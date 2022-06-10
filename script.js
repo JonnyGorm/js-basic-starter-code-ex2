d49dace34d3a34878acc6d8213cdd51c89c7d9b3
jonny_g <jonathangormley23@gmail.com>
2022-06-10 17:33:22 +1000

    Initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+var name = prompt("What is your name?");
+var yob = prompt("What is your year of birth?");
+var age = 2022 - yob;
+
+console.log("Hello, " + name + ". You are " + age + " year's old.");
